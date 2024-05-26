3e9b6a791059121e3a7c05ed0dd0bee7a18ed00a
isadoravrx <isavitoriaxavier@gmail.com>
2024-05-26 17:13:17 -0300

    inserindo atributo id2 nos itens da lista

--- StickerSmash/BooksMenu.js
+++ StickerSmash/BooksMenu.js
@@ -12,110 +12,137 @@ import {
 const DATA = [
   {
     id: 'head',
+    id2 : 'head',
     title: 'Título',
   },
   {
     id: 'head',
+    id2 : 'head',
     title: 'Gênero',
   },
   {
     id: 'head',
+    id2 : 'head',
     title: 'Ano',
   },
   {
     id: 'titulo',
+    id2 : 'A hora da estrela',
     title: 'A hora da Estrela',
   },
   {
     id: 'categoria',
+    id2 : 'A hora da estrela',
     title: 'Romance',
   },
   {
     id: 'ano',
+    id2 : 'A hora da estrela',
     title: '1977',
   },
   {
     id: 'titulo',
+    id2 : 'Dom Casmurro',
     title: 'Dom Casmurro',
   },
   {
     id: 'categoria',
+    id2 : 'Dom Casmurro',
     title: 'Romance',
   },
   {
     id: 'ano',
+    id2 : 'Dom Casmurro',
     title: '1899',
   },
   {
     id: 'titulo',
+    id2 : 'Macunaíma',
     title: 'Macunaíma',
   },
   {
     id: 'categoria',
+    id2 : 'Macunaíma',
     title: 'Romance',
   },
   {
     id: 'ano',
+    id2 : 'Macunaíma',
     title: '1928',
   },
   {
     id: 'titulo',
+    id2 : 'Menino de engenho',
     title: 'Menino de engenho',
   },
   {
     id: 'categoria',
+    id2 : 'Menino de engenho',
     title: 'Ficção',
   },
   {
     id: 'ano',
+    id2 : 'Menino de engenho',
     title: '1932',
   },
   {
     id: 'titulo',
+    id2 : 'O Cortiço',
     title: 'O Cortiço',
   },
   {
     id: 'categoria',
+    id2 : 'O Cortiço',
     title: 'Romance',
   },
   {
     id: 'ano',
+    id2 : 'O Cortiço',
     title: '1890',
   },
   {
     id: 'titulo',
+    id2 : 'Os Sertões',
     title: 'Os Sertões',
   },
   {
     id: 'categoria',
+    id2 : 'Os Sertões',
     title: 'romance-reportagem',
   },
   {
     id: 'ano',
+    id2 : 'Os Sertões',
     title: '1902',
   },
   {
     id: 'titulo',
+    id2 : 'Senhora',
     title: 'Senhora',
   },
   {
     id: 'categoria',
+    id2 : 'Senhora',
     title: 'Romance',
   },
   {
     id: 'ano',
+    id2 : 'Senhora',
     title: '1874',
   },
   {
     id: 'titulo',
+    id2 : 'O Cão Sem Plumas',
     title: 'O Cão Sem Plumas',
   },
   {
     id: 'categoria',
+    id2 : 'O Cão Sem Plumas',
     title: 'Poema',
   },
   {
     id: 'ano',
+    id2 : 'O Cão Sem Plumas',
     title: '1950',
   },
 ];
