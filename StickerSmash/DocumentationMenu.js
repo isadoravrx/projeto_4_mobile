8f228dc22194f93c4e58c7039c945eacf3cfd24f
isadoravrx <isavitoriaxavier@gmail.com>
2024-05-27 08:33:23 -0300

    deixando documentacao com navegacao

--- StickerSmash/DocumentationMenu.js
+++ StickerSmash/DocumentationMenu.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  StyleSheet,
-  Text,
-  StatusBar,
-  TouchableOpacity,
-} from 'react-native';
-
-
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Documento from './documentos';
 const DATA = [
   {
     id: 'head',
@@ -22,17 +14,17 @@ const DATA = [
     title: 'Categoria',
   },
   {
-    id: 'titulo 1',
-    id2: 'Hello World!',
+    id: 'tema',
+    id2: 'Hello_World!',
     title: 'Hello World!',
   },
   {
     id: 'categoria',
-    id2: 'Hello World!',
+    id2: 'Hello_World!',
     title: 'Programação 1',
   },
   {
-    id: 'titulo 2',
+    id: 'tema',
     id2: 'Conceitos POO',
     title: 'Conceitos POO',
   },
@@ -42,7 +34,7 @@ const DATA = [
     title: 'Programação 2',
   },
   {
-    id: 'titulo 3',
+    id: 'tema',
     id2: 'ArrayList',
     title: 'ArrayList',
   },
@@ -52,7 +44,7 @@ const DATA = [
     title: 'Programação 3',
   },
   {
-    id: 'titulo 4',
+    id: 'tema',
     id2: 'Arquitetura em Nuvem',
     title: 'Arquitetura em Nuvem',
   },
@@ -62,7 +54,7 @@ const DATA = [
     title: 'Arq. computadores 2',
   },
   {
-    id: 'titulo 5',
+    id: 'tema',
     id2: 'Autômatos finitos',
     title: 'Autômatos finitos',
   },
@@ -72,7 +64,7 @@ const DATA = [
     title: 'Linguagens Formais',
   },
   {
-    id: 'titulo 6',
+    id: 'tema',
     id2: 'Modelos Conceituais',
     title: 'Modelos Conceituais',
   },
@@ -82,7 +74,7 @@ const DATA = [
     title: 'Banco de Dados 1',
   },
   {
-    id: 'titulo 7',
+    id: 'tema',
     id2: 'Busca em profundidade',
     title: 'Busca em profundidade',
   },
@@ -94,8 +86,8 @@ const DATA = [
 ];
 
 
-const Item = ({title, onPress, itemstyle}) =>(
-  <TouchableOpacity onPress={onPress} style = {itemstyle}>
+const Item = ({ title, onPress, itemstyle }) => (
+  <TouchableOpacity onPress={onPress} style={itemstyle}>
     <Text>{title}</Text>
   </TouchableOpacity>
 );
@@ -104,44 +96,50 @@ const Separator = () => (
   <View style={styles.separator} />
 );
 
-const onPressRow = (id2) => {
-  console.log('Item clicado:', id2); 
-}
+const onPressRow = (id2, navigation) => {
+  navigation.navigate('Documento', { id2 });
+};
 
-const renderItem = ({ item }) => {
+const renderItem = ({ item, navigation }) => {
   // Define o estilo condicionalmente
   const textStyle = 
-  item.id === 'head' ? styles.header :
+  item.id === 'tema' ? styles.header :
   item.id === 'categoria' ? styles.categoria:
   styles.title;
   // const anoStyle = item.id === 'ano' ? styles.contain : styles.title;
 
   return (
     <View style={styles.item}>
-        <Item title={item.title} onPress={() => onPressRow(item.id2)} itemstyle={textStyle}/>
+      <Item title={item.title} onPress={() => onPressRow(item.id2, navigation)} itemstyle={textStyle} />
     </View>
   );
 };
 
 
-
-
-const App = () => {
+const DocumentList = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        numColumns= {2}
-        ItemSeparatorComponent = {Separator}
+        numColumns={2}
+        ItemSeparatorComponent={Separator}
         data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
+        renderItem={({ item }) => renderItem({ item, navigation })}
+        keyExtractor={item => item.id2}
       />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
+const Stack = createStackNavigator();
 
+const DocumentsMenu = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="DocumentList" component={DocumentList} options={{ title : 'Documentos' }}  />
+    <Stack.Screen name="Documento" component={Documento} options={{ title : 'Detalhes do Documento' }}  />
+  </Stack.Navigator>
+);
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
@@ -156,7 +154,6 @@ const styles = StyleSheet.create({
     flexBasis: 0,
     justifyContent: 'center'
   },
-
   categoria: {
     borderWidth: 1,
     borderRadius: 4,
@@ -168,7 +165,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexBasis: 0
   },
-
   ano: {
     width: 50,
     borderWidth: 1,
@@ -181,7 +177,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexBasis: 0
   },
-
   header: {
     marginBottom: 2,
     fontSize: 14,
@@ -192,7 +187,6 @@ const styles = StyleSheet.create({
     color: '#999',
     justifyContent: 'center'
   },
-
   title: {
     fontSize: 12
   },
@@ -204,4 +198,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default DocumentsMenu;
--- StickerSmash/documentos.js
+++ StickerSmash/documentos.js
@@ -1,43 +1,35 @@
 import React from 'react';
 import { SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity, Linking } from 'react-native';
 
-const Livro = ({ route, navigation }) => {
+const Documento = ({ route, navigation }) => {
   const { id2 } = route.params;
 
-  const livrosDetalhes = {
-    'A_hora_da_estrela': {
-      title: 'A hora da Estrela',
+  const documentosDetalhe = {
+    'Hello_World!': {
+      title: 'Hello World!',
       image: require('./images/livros/a_hora_da_estrela.webp'),
       descricao: 'É o último romance da escritora brasileira Clarice Lispector, publicado em 1977. Trata-se de uma obra instigante e original, de cunho autobiográfico, pertencente à Terceira Geração Modernista. É classificada como um romance intimista, também conhecido como romance psicológico, estilo em que a autora se destaca. Afinal, a obra de Clarice é marcada por suas emoções e sentimentos pessoais.',
       autor: 'Clarice Lispector',
       preco: '124,90',
       link: 'https://proj2-web-mobile.vercel.app/livros.html',
     },
-    'Dom_Casmurro': {
-      title: 'Dom Casmurro',
-      image: require('./images/livros/dom_casmurro.webp'),
-      descricao: 'Em Dom Casmurro, o narrador Bento Santiago retoma a infância que passou na Rua de Matacavalos e conta a história do amor e das desventuras que viveu com Capitu, uma das personagens mais enigmáticas e intrigantes da literatura brasileira. Nas páginas deste romance, encontra-se a versão de um homem perturbado pelo ciúme, que revela aos poucos sua psicologia complexa e enreda o leitor em sua narrativa ambígua acerca do acontecimento ou não do adultério da mulher com olhos de ressaca, uma das maiores polêmicas da literatura brasileira.',
-      autor: 'Machado de Assis',
-      preco: '24,90',
-      link: 'https://proj2-web-mobile.vercel.app/livros.html',
-    },
   };
 
-  const livro = livrosDetalhes[id2];
-  navigation.setOptions({ title: livro.title });
+  const documento = documentosDetalhe[id2];
+  navigation.setOptions({ title: documento.title });
 
   const openLink = () => {
-    Linking.openURL(livro.link).catch(err => console.error('Erro ao abrir o URL:', err));
+    Linking.openURL(documento.link).catch(err => console.error('Erro ao abrir o URL:', err));
   };
 
   return (
     <View style={styles.container}>
-      <Image source={livro.image} style={styles.image} />
-      <Text style={styles.descricao}>{livro.descricao}</Text>
+      <Image source={documento.image} style={styles.image} />
+      <Text style={styles.descricao}>{documento.descricao}</Text>
       <Text style={styles.autorstyle}>
-        <Text style={{ fontWeight: "bold" }}>Autor(a): {livro.autor}</Text>
+        <Text style={{ fontWeight: "bold" }}>Autor(a): {documento.autor}</Text>
       </Text>
-      <Text style={styles.preco}>R$ {livro.preco}</Text>
+      <Text style={styles.preco}>R$ {documento.preco}</Text>
       <TouchableOpacity style={styles.button} onPress={openLink}>
         <Text style={styles.buttonText}>Link para acesso</Text>
       </TouchableOpacity>
@@ -81,4 +73,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Livro;
+export default Documento;
